72e0bbdf573f9f7898ead6e2552ba5a70101dc25
Viki-Script <vigneshg0405@gmail.com>
2025-07-19 13:17:29 +0530

    Added completion,sorting function

--- TodoMate/src/components/TodoItem.jsx
+++ TodoMate/src/components/TodoItem.jsx
@@ -5,9 +5,20 @@ export default function TodoItem({item,todos,setTodos}){
         console.log("deleteButton Clicked",item)
         setTodos(todos.filter((todo)=> todo !== item))
     }
+
+    function handleClick(name){
+        const newArray = 
+        todos.map((todo)=>todo.name === name ?{...todo,done:!todo.done}: todo
+        )
+        setTodos(newArray)
+    }
+
+    const  className = item.done?styles.completed:""
     return <div className={styles.item}>
         <div className={styles.itemName}>
-            {item}
+            <span className={className} onClick={()=> handleClick(item.name)}>
+                {item.name}
+            </span>
             <span>
                 <button onClick={()=>handleDelete(item)} className={styles.deleteButton}>X</button>
             </span>
